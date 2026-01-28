8f1ff1af83ba67f62e153deb7d522e31402128f8
Sonia_Sandler <ssandler@redhat.com>
2026-01-27 21:32:25 -0500

    chore: check test
    Signed-off-by: Sonia_Sandler <ssandler@redhat.com>

--- src/create-cluster.spec.ts
+++ src/create-cluster.spec.ts
@@ -349,15 +349,10 @@ test('expect cluster to be created with ports as numbers', async () => {
     expect.anything(),
     expect.stringContaining(`
   - containerPort: 80
-
     hostPort: 9091
-
     protocol: TCP
-
   - containerPort: 443
-
     hostPort: 9444
-    
     protocol: TCP`),
     expect.anything(),
   );
